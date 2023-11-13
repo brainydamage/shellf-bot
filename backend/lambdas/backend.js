0d9b9524edcfdb1b105306b1b0baf9f08d3e6b37
ulyana andreeva <brainydamage@gmail.com>
2023-11-13 01:19:33 +0100

    website is working!

--- backend/lambdas/backend.js
+++ backend/lambdas/backend.js
@@ -1,23 +1,88 @@
 'use strict';
+const {SSMClient, GetParameterCommand} = require("@aws-sdk/client-ssm");
+const {google} = require('googleapis');
+
+const ssmClient = new SSMClient({region: "eu-central-1"});
+
+async function getParameter(name, withDecryption = false) {
+  const command = new GetParameterCommand({
+    Name: name, WithDecryption: withDecryption
+  });
+
+  const response = await ssmClient.send(command);
+  return response.Parameter.Value;
+}
 
 module.exports.handler = async (event) => {
-  const body = JSON.parse(event.body);
   console.log('backend handler triggered');
-  console.log(JSON.stringify(body));
+  // console.log(event);
+  const requestedBookID = event.pathParameters.bookID;
+  console.log(`bookID requested = ${requestedBookID}`);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  const clientEmail = await getParameter('shellf_client_email');
+  const privateKey = (await getParameter('shellf_private_key', true)).replace(
+    /\\n/g, '\n');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY
-  // integration return { message: 'Go Serverless v1.0! Your function executed
-  // successfully!', event };
+  const client = new google.auth.JWT(clientEmail, null, privateKey,
+    ['https://www.googleapis.com/auth/spreadsheets']);
+
+  const sheets = google.sheets({version: 'v4', auth: client});
+  const spreadsheetId = '1fbuYSBH3QIZgGX_bPuuLlXOJOBQ4mNKuev6zXDQYzhc';
+  const range = `booksDB`;
+
+  let bookTitle = "";
+  let bookAuthor = "";
+
+  try {
+    const response = await sheets.spreadsheets.values.get(
+      {spreadsheetId, range});
+    const rows = response.data.values;
+    console.log(`found ${rows.length}`);
+
+    if (rows && rows.length > 0 && requestedBookID !== 0) {
+      const row = rows[requestedBookID];
+      const bookID = row[0];
+
+      if (bookID === requestedBookID) {
+        bookTitle = row[1];
+        bookAuthor = row[2];
+      } else {
+        console.log(`!!! book is on the wrong place, searching...`);
+
+        console.log(`bookID = ${bookID}`);
+        console.log(`requestedBookID = ${requestedBookID}`);
+
+        for (const row of rows) {
+          if (row[0] === requestedBookID) {
+            bookTitle = row[1];
+            bookAuthor = row[2];
+          }
+        }
+      }
+
+      console.log(`${bookTitle} by ${bookAuthor}`);
+    }
+
+  } catch (error) {
+    console.log(`failed to access database`);
+    console.log(error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({message: "Error accessing database"}),
+    };
+  }
+
+  return (bookTitle !== '' && bookAuthor !== '') ?
+    {
+      statusCode: 200,
+      body: JSON.stringify({
+        title: bookTitle,
+        author: bookAuthor,
+      }, null, 2),
+    } :
+    {
+      statusCode: 400,
+      body: JSON.stringify({message: "Book not found"}, null, 2),
+    };
 };
--- frontend/public/bookDetails.js
+++ frontend/public/bookDetails.js
@@ -1,42 +1,51 @@
-window.onload = function() {
-  const bookID = new URL(window.location.href).pathname.split("/")[2];
+window.onload = function () {
+  const gwUrl = 'https://y1jyo2lk58.execute-api.eu-central-1.amazonaws.com';
+  const pathParts = new URL(window.location.href).pathname.split("/");
+  const bookID = pathParts.length > 2 ? pathParts[2] : null;
 
-  fetch(`/api/book/${bookID}`)
-    .then(response => {
-      if (!response.ok) {
-        console.log(response);
-        throw new Error('Network response was not ok: ' + response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      // console.log(data);
-      // Update the webpage with the book details
-      // Example: document.getElementById('book-info').innerText = data.title;
-      document.getElementById('loading-message').style.display = 'none';
-      document.getElementById('loader').style.display = 'none';
+  if (bookID) {
+    fetch(`${gwUrl}/book/${bookID}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('An error occurred: ' + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Update the webpage with the book details
+        // Example: document.getElementById('book-info').innerText = data.title;
+        document.getElementById('loading-message').style.display = 'none';
+        document.getElementById('loader').style.display = 'none';
 
-      document.getElementById('book-title').textContent = data.title;
-      document.getElementById('book-author').textContent = data.author;
+        document.getElementById('book-title').textContent = data.title;
+        document.getElementById('book-author').textContent = data.author;
 
-      document.getElementById('book-info').style.display = 'block';
-      document.getElementById('borrow-button').style.display = 'block';
+        document.getElementById('book-info').style.display = 'block';
+        document.getElementById('borrow-button').style.display = 'block';
 
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      document.getElementById('book-info').style.display = 'block';
-      // document.getElementById('book-author').style.display = 'block';
-      document.getElementById('help-button').style.display = 'block';
+      })
+      .catch(error => {
+        // console.error('Error:', error);
+        document.getElementById('book-info').style.display = 'block';
+        document.getElementById('help-button').style.display = 'block';
 
-      document.getElementById('book-author').textContent = 'не могу найти книгу (';
-      // document.getElementById('loading-message').textContent = 'не получилось найти книгу (';
+        document.getElementById('book-author').textContent =
+          'не могу найти книгу (';
 
-      document.getElementById('loading-message').style.display = 'none';
-      document.getElementById('loader').style.display = 'none';
-    });
+        document.getElementById('loading-message').style.display = 'none';
+        document.getElementById('loader').style.display = 'none';
+      });
+  } else {
+    document.getElementById('book-info').style.display = 'block';
+    document.getElementById('help-button').style.display = 'block';
+
+    document.getElementById('book-author').textContent = 'что-то пошло не так';
+
+    document.getElementById('loading-message').style.display = 'none';
+    document.getElementById('loader').style.display = 'none';
+  }
 };
 
-document.getElementById('help-button').onclick = function() {
+document.getElementById('help-button').onclick = function () {
   window.open('https://t.me/shellllllf', '_blank');
 };
