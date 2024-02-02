883f775590220137ccf8bc5abcf5c67b0af13e48
ulyana andreeva <brainydamage@gmail.com>
2024-02-02 03:16:36 +0100

    implement button, search, redirection

--- library/public/bookList.js
+++ library/public/bookList.js
@@ -1,56 +1,124 @@
-window.onload = function () {
-  const gwUrl = 'https://y1jyo2lk58.execute-api.eu-central-1.amazonaws.com';
-  const pathParts = new URL(window.location.href).pathname.split("/");
-  const bookID = pathParts.length > 2 ? pathParts[2] : null;
+function searchBooks() {
+  const input = document.getElementById('search-input');
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("books-table");
+  const tr = table.getElementsByTagName("tr");
 
-  if (bookID) {
-    fetch(`${gwUrl}/book/${bookID}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('An error occurred: ' + response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        // Update the webpage with the book details
-        // Example: document.getElementById('book-info').innerText = data.title;
-        document.getElementById('loading-message').style.display = 'none';
-        document.getElementById('loader').style.display = 'none';
+  for (let i = 1; i < tr.length; i++) {
+    const td = tr[i].getElementsByTagName("td");
+    if (td.length > 1) { // Ensure there are enough <td> elements
+      const title = td[0].getElementsByClassName(
+        "book-title")[0].textContent.toUpperCase();
+      const author = td[0].getElementsByClassName(
+        "book-author")[0].textContent.toUpperCase();
+      const shelf = td[1].textContent.toUpperCase(); // Get the shelf name from
+                                                     // the second <td>
 
-        document.getElementById('book-title').textContent = data.title;
-        document.getElementById('book-author').textContent = data.author;
-
-        document.getElementById('book-info').style.display = 'block';
-        document.getElementById('borrow-button').style.display = 'block';
-
-      })
-      .catch(() => {
-        document.getElementById('book-info').style.display = 'block';
-        document.getElementById('help-button').style.display = 'block';
-
-        document.getElementById('book-author').textContent =
-          'не могу найти книгу (';
-
-        document.getElementById('loading-message').style.display = 'none';
-        document.getElementById('loader').style.display = 'none';
-      });
-  } else {
-    document.getElementById('book-info').style.display = 'block';
-    document.getElementById('help-button').style.display = 'block';
-
-    document.getElementById('book-author').textContent = 'что-то пошло не так';
-
-    document.getElementById('loading-message').style.display = 'none';
-    document.getElementById('loader').style.display = 'none';
+      // Check if the row should be displayed based on title, author, or shelf
+      if (title.indexOf(filter) > -1 || author.indexOf(filter) > -1 ||
+        shelf.indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
   }
-};
+}
 
-document.getElementById('help-button').onclick = function () {
-  window.open('https://t.me/shellllllf', '_blank');
-};
+document.getElementById('search-input')
+  .addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      // Prevent the default action to avoid submitting the form
+      event.preventDefault();
+      // Remove focus from the input field
+      this.blur();
+    }
+  });
 
-document.getElementById('borrow-button').onclick = function () {
-  const pathParts = new URL(window.location.href).pathname.split("/");
-  const bookID = pathParts.length > 2 ? pathParts[2] : 'no_id';
-  window.location = `https://t.me/shellf_bot?start=${bookID}`;
-};
+document.addEventListener("DOMContentLoaded", function () {
+  const rows = document.querySelectorAll('#books-table tr');
+
+  rows.forEach(function (row) {
+    row.addEventListener('touchstart', function () {
+      this.classList.add('row-tap-feedback');
+    });
+
+    row.addEventListener('touchend', function () {
+      // Use setTimeout to leave the feedback class long enough to be noticed
+      setTimeout(() => this.classList.remove('row-tap-feedback'), 200);
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const rows = document.querySelectorAll('#books-table tr');
+  let currentButtonRow = null; // Keep track of the current button row
+
+  // Listen for clicks anywhere in the document
+  document.addEventListener('click', function (event) {
+    // If there's a button row and the click is outside of it, remove the
+    // button row
+    if (currentButtonRow && !currentButtonRow.contains(event.target) &&
+      !event.target.closest('#books-table tr')) {
+      currentButtonRow.remove();
+      currentButtonRow = null; // Reset the current button row tracker
+    }
+  }, true); // Capture phase to handle the event before it propagates
+
+  rows.forEach(function (row, index) {
+    // Skip the header row
+    if (index > 0) {
+      row.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent the click from reaching the
+                                 // document listener
+
+        // Remove styles from previously clicked row
+        const previousClickedRow = document.querySelector('.clicked-row');
+        if (previousClickedRow) {
+          previousClickedRow.classList.remove('clicked-row');
+        }
+
+        // Mark the current row as clicked
+        this.classList.add('clicked-row');
+
+        // Remove any existing button row
+        if (currentButtonRow) {
+          currentButtonRow.remove();
+        }
+
+        // Create a new button row
+        const buttonRow = document.createElement('tr');
+        buttonRow.className = 'redirect-button-row';
+        const buttonCell = document.createElement('td');
+        buttonCell.colSpan = this.cells.length; // Span across all columns
+        const button = document.createElement('button');
+        button.textContent = 'подписаться на книгу';
+        button.className = 'redirect-button';
+        button.addEventListener('click', function (event) {
+          event.stopPropagation(); // Prevent triggering row's click event
+          window.location.href = row.getAttribute('data-href'); // Redirect
+        });
+
+        // Append the button to the cell, and the cell to the row
+        buttonCell.appendChild(button);
+        buttonRow.appendChild(buttonCell);
+
+        // Insert the new button row directly after the clicked row
+        if (row.nextSibling) {
+          row.parentNode.insertBefore(buttonRow, row.nextSibling);
+        } else {
+          // If the clicked row is the last one, append the button row at the
+          // end
+          row.parentNode.appendChild(buttonRow);
+        }
+
+        setTimeout(() => button.style.opacity = '1', 5); // Delay of 1
+                                                         // millisecond
+
+        // Track the current button row
+        currentButtonRow = buttonRow;
+      });
+    }
+  });
+});
+
